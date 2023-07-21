c3fcde6e0c17fa1e580d533dd9e328e69be4d1e3
Tachyon007 <81450862+Tachyon007@users.noreply.github.com>
2023-07-21 16:31:12 -0400

    Add files via upload

---
+++ Hackathon Frontend/main.js
@@ -0,0 +1,50 @@
+
+
+//clock
+let clock = document.getElementById("clock");
+function setTwoChar(inText){
+    if(inText.length < 2){
+        inText = "0" + inText;
+    }
+    return inText;
+}
+setInterval(()=>{
+    let currTime = new Date();
+    let hour = setTwoChar("" + (currTime.getHours() % 12));
+    let min = setTwoChar("" + currTime.getMinutes());
+    let sec = setTwoChar("" + currTime.getSeconds());
+
+    clock.innerHTML = hour + " : " + min + " : " + sec;
+
+})
+
+
+//grid menu
+function scrollToFilter(){
+    document.getElementById("filter").scrollIntoView({ behavior: "smooth"});
+}
+
+
+
+//tag selection
+
+let selectedTags = [];
+
+function toggleSelect(el){
+    let classList = el.classList;
+    if(classList.contains("tagSelectionSelected")){
+        classList.remove("tagSelectionSelected");
+        selectedTags = selectedTags.filter((tag)=>{
+            return (tag != el.innerHTML)
+        })
+    }else{
+        classList.add("tagSelectionSelected");
+        selectedTags.push(el.innerHTML);
+    }
+
+    console.log(selectedTags);
+}
+
+function search(){
+    alert(selectedTags);
+}
